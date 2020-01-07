e6b3db0f0e53cdafa960c91f5a856b0d9b79e3fe
Nick Buchert Laptop <nicklt80@gmail.com>
2020-01-07 13:45:42 -0600

    Custom widgets and state management.
    -Nick Buchert

--- flutter_course/lib/main.dart
+++ flutter_course/lib/main.dart
@@ -1,45 +1,19 @@
 import 'package:flutter/material.dart';
+import './product_manager.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
-}
 
-class _MyAppState extends State<MyApp> {
-
-  List<String> products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Easy List'),
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  child: Text('Add Product'),
-                  onPressed: () {},
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/pizza_ranch.png'),
-                    Text('Pizza Ranch')
-                  ],
-                ),
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('Easy List'),
+        ),
+        body: ProductManager('Food Tester'),
+      ),
     );
   }
 }
---
+++ flutter_course/lib/product_manager.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import './products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = []; // underscore for properties only used in this class...
+
+  @override
+  void initState() {
+    _products.add(widget.startingProduct); // use widget property to access properties
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(10.0),
+        child: RaisedButton(
+          child: Text('Add Product'),
+          onPressed: () {
+            setState(() {
+              _products.add('Advanced Food Tetster');
+            });
+          },
+        ),
+      ),
+      Products(_products)
+    ]);
+  }
+}
